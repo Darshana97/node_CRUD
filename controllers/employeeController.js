f7d70176c2ba8437c9b98e42699a0f598336a6b3
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-04-27 17:05:35 +0530

    list

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
 var router = express.Router();
+const mongoose = require("mongoose");
+const Employee = mongoose.model("Employee");
 
 router.get("/", (req, res) => {
   res.render("employee/addOrEdit", {
@@ -9,7 +11,26 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  console.log(req.body);
+  insertRecord(req, res);
+});
+
+function insertRecord(req, res) {
+  var employee = new Employee();
+  employee.fullName = req.body.fullName;
+  employee.email = req.body.email;
+  employee.mobile = req.body.mobile;
+  employee.city = req.body.city;
+  employee.save((err, doc) => {
+    if (!err) {
+      res.redirect("employee/list");
+    } else {
+      console.log("Error during record insertion : " + err);
+    }
+  });
+}
+
+router.get("/", (req, res) => {
+  res.json("from list");
 });
 
 module.exports = router;
