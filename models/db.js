e342799dd5256a14fd266fcb01882ac20b28e77c
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-04-27 12:47:08 +0530

    mongodb connect

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect(
   "mongodb://localhost:27017/EmployeeDB",
-  { useNewUrlParser: true },
+  { useNewUrlParser: true, useUnifiedTopology: true },
   (err) => {
     if (!err) {
       console.log("Mongodb conncetion succeeded");
