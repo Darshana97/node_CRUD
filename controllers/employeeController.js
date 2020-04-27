7e6f9b454b08f3493933fd1485fd4e425d8bd13b
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-04-27 21:58:02 +0530

    create list

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -37,8 +37,16 @@ function insertRecord(req, res) {
   });
 }
 
-router.get("/", (req, res) => {
-  res.json("from list");
+router.get("/list", (req, res) => {
+  Employee.find((err, docs) => {
+    if (!err) {
+      res.render("employee/list", {
+        list: docs,
+      });
+    } else {
+      console.log("Error in retrieving employee list: " + err);
+    }
+  });
 });
 
 function handleValidationError(err, body) {
