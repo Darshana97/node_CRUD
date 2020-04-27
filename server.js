f878ebb0c377ecd3a14ba0c0052efa797ec8ff9c
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-04-27 16:55:45 +0530

    get data

--- server.js
+++ server.js
@@ -3,12 +3,21 @@ require("./models/db");
 const express = require("express");
 const path = require("path");
 const exphbs = require("express-handlebars");
+const bodyparser = require("body-parser");
 
 const employeeController = require("./controllers/employeeController");
 
 var app = express();
+app.use(
+  bodyparser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(bodyparser.json());
 
 app.set("views", path.join(__dirname, "/views/"));
+
 app.engine(
   "hbs",
   exphbs({
@@ -17,6 +26,8 @@ app.engine(
     layoutsDir: __dirname + "/views/layouts/",
   })
 );
+
+app.set("view engine", "hbs");
 
 app.listen(5300, () => {
   console.log("Express server started at port : 5300");
